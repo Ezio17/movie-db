80e2e6c08ba81331b8308eacd6030a83e94382b4
m.shcherba <mykola.shcherba@snagasolutions.com>
2025-07-12 20:50:14 +0300

    CI test

--- app.vue
+++ app.vue
@@ -2,6 +2,7 @@
   <div>main!</div>
   <p class="text-center">Count: {{ counter.count }}</p>
   <p>Double: {{ counter.doubleCount }}</p>
+  <p>Fourth: {{ counter.doubleCount * 2 }}</p>
   <button @click="counter.increment()">Increment</button>
   <Button />
 </template>
